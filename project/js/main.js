a0709de37db1f82e64756cd5dbe5388b1a2d01da
Avani Aggrwal <avaniaggrwal@dhcp-rhodes-2922.eduroam.cornell.edu>
2018-09-15 13:42:12 -0400

    added my own touch

--- project/js/main.js
+++ project/js/main.js
@@ -9,3 +9,32 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
+function myAboutFunction() {
+  // use jQuery ($ is shorthand) to find the div on the page and then change the html
+  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+
+  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+
+  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+}
+
+function myContactFunction() {
+  // use jQuery ($ is shorthand) to find the div on the page and then change the html
+  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+
+  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+
+  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+}
+
+function myResumeFunction() {
+  // use jQuery ($ is shorthand) to find the div on the page and then change the html
+  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+
+  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+
+  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+}
